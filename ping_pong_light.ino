5c8b57a0604a2cc0d7278aa83b21de669e896bd8
Jeremy Wilson <jerwil@gmail.com>
2013-05-25 22:32:35 -0500

    Cleaned up code and completed comments

--- ping_pong_light.ino
+++ ping_pong_light.ino
@@ -24,8 +24,6 @@ unsigned long currentTime;
 double second_timer[1] = {0}; // This is use dto keep track of the timer used to tick for each second
 int ticked = 0;
 
-int current_color[1] = {0}; // This is used by the color change function
-
 int color = 0;
 
 void setup() {                
@@ -39,61 +37,34 @@ void setup() {
 void loop() {
   
   button_state = digitalRead(buttonPin);
-  button_pushed = button_press (button_state, button_press_initiate, button_press_completed);
-  if (button_pushed == 1){
+  button_pushed = button_press (button_state, button_press_initiate, button_press_completed);    // This function looks for a complete button press of press and release
+  if (button_pushed == 1){                // Change mode whenever a button press is detected
     mode += mode_adjust;
   }
  
- if (mode >= 0 && mode < 1){
- mode_adjust = 0.2;
+ if (mode >= 0 && mode < 1){              // This is color changing mode. As the button is pressed, the colors change more slowly
+ mode_adjust = 0.2;                       // Modes 1.2, 1.4 etc are sub-modes with different color change speeds
   if (tick(delay_int*(mode),second_timer) == 1){
         color = color + 1;
   }
-  if (color > 6){ color = 0; }
+  if (color > 6){ color = 0; }            // Reset back to red
   display_color(color);  
  }
  
  if (mode >= 1){
 mode_adjust = 1;
 
-if (tick(delay_int,second_timer) == 1){
+if (tick(delay_int,second_timer) == 1){ // This flashes a specific color depending on the mode. There are 6 different colors
  if( ticked == 1){ ticked = 0; }
  else if (ticked == 0){ ticked = 1; }
  }
 if (ticked == 0) { display_color(mode); }
-else if (ticked == 1) { display_color(7); }
+else if (ticked == 1) { display_color(7); }  // Color 7 is no color (no LEDs lit)
  }
  
- if (mode > 7) {mode = 0;}
-  
-
-  
-
- 
-//  digitalWrite(redPin, colors[color,0]);
-//  digitalWrite(greenPin, colors[color,1]);
-//  digitalWrite(bluePin, colors[color,2]);  
-    
-
-}
-//  digitalWrite(redPin, LOW);
-//  digitalWrite(greenPin, LOW);
-//  digitalWrite(bluePin, LOW);  
-//  delay(delay_int);     
-//  digitalWrite(redPin, LOW);
-//  digitalWrite(greenPin, HIGH);
-//  digitalWrite(bluePin, HIGH);
-//  delay(delay_int);
-//  digitalWrite(redPin, HIGH);
-//  digitalWrite(greenPin, LOW);
-//  digitalWrite(bluePin, HIGH);
-//  delay(delay_int);       
-//  digitalWrite(redPin, HIGH);
-//  digitalWrite(greenPin, HIGH);
-//  digitalWrite(bluePin, LOW);
-//  delay(delay_int);         
+ if (mode > 7) {mode = 0;} // The modes cycle through as the button is pressed
           
-
+}
 
 int button_press (int button_indicator, int button_press_initiated[1], int button_press_complete[1]){
 	if (button_indicator == 0 && button_press_initiated[0] == 1) {
@@ -117,7 +88,7 @@ if(currentTime >= (timekeeper[0] + delay)){
 else {return 0;}
 }
 
-void display_color(int color){
+void display_color(int color){ // This function creates colors by combining colors together. No PWM, just digital.
 if (color == 0){  
   digitalWrite(redPin, LOW);
   digitalWrite(greenPin, LOW);
